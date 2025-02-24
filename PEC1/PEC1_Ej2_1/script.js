634bab396abe44514940421ae87708f466c77211
Oriol Cardona Alsina <ocardonaa@uoc.edu>
2025-02-24 18:50:42 +0100

    edad y nueva password añadidos

--- PEC1/PEC1_Ej2_1/script.js
+++ PEC1/PEC1_Ej2_1/script.js
@@ -3,6 +3,7 @@ const username = document.getElementById('username');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
+const age = document.getElementById('age');
 
 function showError(input, message) {
     const formControl = input.parentElement;
@@ -26,19 +27,41 @@ function checkEmail(input) {
     }
 }
 
-function checkPasswordsMatch(password1, password2) {
-    if(password1.value !== password2.value) {
-        showError(password2, 'Passwords do not match');
+function checkLenght(input, min, max) {
+    if(input.value.length < min || input.value === '') {
+        showError(input, `${getFieldName(input)} must be at least ${min} characters long`);
+    }
+    else if (input.value.length > max) {
+        showError(input, `${getFieldName(input)} must be at less than ${max} characters long`);
     }
     else {
         showSuccess(input);
     }
 }
 
+function checkCorrectPassword(input) {
+    const regexPassword = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[ `~!@#$%^&*()_+\-={}|[\]\\:";'<>?,./]).{8,}$/;
+    if(regexPassword.test(input.value.trim())) {
+        showSuccess(input);
+    }
+    else {
+        showError(input, `${getFieldName(input)} is invalid`);
+    }
+}
+
+function checkPasswordsMatch(password1, password2) {
+    if(password1.value !== password2.value || password1.value === '') {
+        showError(password2, 'Passwords do not match');
+    }
+    else {
+        showSuccess(password1);
+    }
+}
+
 function checkRequired(inputArr) {
     inputArr.forEach(input => {
         if(input.value.trim() === '') {
-            showError(input, `${getFieldName(input)} is required`)
+            showError(input, `${getFieldName(input)} is required`);
         }
         else {
             showSuccess(input);
@@ -50,21 +73,38 @@ function getFieldName(input) {
     return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
 
-function checkLenght(input, min, max) {
-    if(input.value.length < min) {
-        showError(input, `${getFieldName(input)} must be at least ${min} characters long`)
+function checkAge(input) {
+    if(input.value !== '') {
+        const convertedAge = Number(input.value);
+        if(!isNaN(convertedAge) && input.value !== '') {
+            if(convertedAge > 999) {
+                showError(input, `${getFieldName(input)} cannot be greater than 1000 years`);
+            }
+            else if (convertedAge < 0) {
+                showError(input, `${getFieldName(input)} cannot be lower than 0 years`);
+            }
+            else {
+                showSuccess(input);
+            }
+        }
+        else {
+            showError(input, `${getFieldName(input)} must be a number`);
+    
+        }
     }
-    else if (input.value.length > max) {
-        showError(input, `${getFieldName(input)} must be at less than ${max} characters long`)
+    else {
+        showError(input, `${getFieldName(input)} is required`);
     }
+    
 }
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
-    checkRequired([username, email, password, password2]);
+    checkRequired([username, email, password2]);
     checkLenght(username, 3, 15);
-    checkLenght(password, 6, 25);
+    checkCorrectPassword(password);
     checkEmail(email);
     checkPasswordsMatch(password, password2);
+    checkAge(age);
 });
--- PEC1/README.md
+++ PEC1/README.md
@@ -6,11 +6,14 @@ ocardonaa
 Oriol Cardona Alsina
 
 ## Dificultades y ejercicios realizados en esta PEC
-**21/02**
 Dudas sobre algunos conceptos de Git, como git rebase, git revert pero nada realmente complicado, revisandolo mejor y comprovando la documentación oficial es viable.
 
 Realizando el ejercicio 1, las 4 primeras preguntas consulté las características de TypeScript, ya que no tengo experiencia con este y descubrí nuevas APIs que no conocia mirando foros y páginas web.
 
 Continuando con la realización del ejercicio 1 pude aprender mucho sobre los preprocesadores, sourcemaps y transpiladores.
 
-Para acabar el ejercicio 1, en la última parte pude ver algunos productos que son competencia de Github, ya que yo mayoritariamente he trabajado con este. También me costó pillar la parte de cita 2 módulos pero al final lo pude entender bien. Y aprendí sobre WebPack
+Para acabar el ejercicio 1, en la última parte pude ver algunos productos que son competencia de Github, ya que yo mayoritariamente he trabajado con este. También me costó pillar la parte de cita 2 módulos pero al final lo pude entender bien. Y aprendí sobre WebPack.
+
+Ya en el ejercicio 2 con el Form pude refrescar mucho contenido sobre como hacer forms en html y css y repasar tambien el método getElementById y sus utilidades. La parte más compleja fue encontrar un buen regex definitivo para comprovar la validez del email.
+
+Haciendo la comprovación de la edad tuve que asegurarme que no se introduce algo que no es un número y convertirlo correctamente a número y gestionar si la conversión a número no ha funcionado (dando un NaN porque se ha introducido texto) o si ha sido satisfactoria y hemos de comprovar si la edad está comprendida entre 0 y 1000 años. El segundo problema es que convertir el string vacío (''), provoca que sea un 0, por lo que no introducir nada es válido pero no queremos eso, por lo que hay que volver a comprovar el campo input.value !== ''
